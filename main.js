d26fccb3d417e8f7883820b7f95ac8e66439cd7d
sonakallacheril <76269703+sonakallacheril@users.noreply.github.com>
2021-03-17 21:28:24 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function toggle() {
+    var text = document.getElementById("display_info");
+    if (text.style.display === "block") {
+      text.style.display = "none";
+    } else {
+      text.style.display = "block";
+    }
+  }
+
+  function toggle2() {
+    var text = document.getElementById("display2_info");
+    if (text.style.display === "block") {
+      text.style.display = "none";
+    } else {
+      text.style.display = "block";
+    }
+  }
+
+  function toggle3() {
+    var text = document.getElementById("display3_info");
+    if (text.style.display === "block") {
+      text.style.display = "none";
+    } else {
+      text.style.display = "block";
+    }
+  }
+
